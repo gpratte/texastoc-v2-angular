0cdb078cd75fc5701980c0db05d1e824051fe5c2
Gil Pratte <gil.pratte@mutualmobile.com>
2018-06-13 08:55:33 -0500

    select exiting players from list

--- src/app/game/existing-player/existing-player.component.ts
+++ src/app/game/existing-player/existing-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../../player/model/player';
 
 @Component({
   selector: 'app-existing-player',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./existing-player.component.css']
 })
 export class ExistingPlayerComponent implements OnInit {
+
+  players = [
+    new Player(1, 'Tom', undefined, undefined, 5),
+    new Player(2, 'Dick', undefined, undefined, 5),
+    new Player(3, 'Harry', undefined, undefined, 4),
+    new Player(3, 'Ron', undefined, undefined, 2),
+    new Player(3, 'Hermione', undefined, undefined, 1),
+    new Player(3, 'Draco'),
+    new Player(3, 'Morpheous'),
+    new Player(3, 'Neo'),
+    new Player(3, 'Snape'),
+    new Player(3, 'Trinity')
+  ]
 
   constructor() { }
 
